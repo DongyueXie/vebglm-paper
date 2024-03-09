ba04332be2f8a967115ce5f514602d0d9d421856
DongyueXie <dongyxie@gmail.com>
2024-03-09 11:45:58 -0800

    revise simu code

--- simulation/main_binary.py
+++ simulation/main_binary.py
@@ -1,7 +1,7 @@
 import sys
 import os
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
-sys.path.append('/home/dxie/myPy/VEBGLM/src')
+sys.path.append('/home/mypro3/myPy/VEBGLM/src')
 from VEBGLM import VEBGLM
 import numpy as np
 import pandas as pd
@@ -17,7 +17,7 @@ from methods.varbvsR import varbvsR
 import argparse
 import pickle
 from utils_func import generate_train_data_binary,generate_test_data_binary,gen_beta,X_generator
-
+import timeit
 
 
 
@@ -32,16 +32,18 @@ def main(args):
     # target_var = args.target_var
     # Define models and metrics
     # bayesregR(),
-    models = [VEBGLM(prior='ash'),VEBGLM(prior='point_normal'),VEBGLM(prior='point_laplace'),
-              VEBGLM(prior='ash'),VEBGLM(prior='point_normal'),VEBGLM(prior='point_laplace'),
-              VEBGLM(prior='ash'),VEBGLM(prior='point_normal'),VEBGLM(prior='point_laplace'),
-              VEBGLM(prior='ash'),VEBGLM(prior='point_normal'),VEBGLM(prior='point_laplace'),
-              ] 
-            #   glmnetR(penalty="lasso"),elasticnetR(),
-            #   glmnetR(penalty="ridge"),
-            #   L0Learn(),
-            #   ncvregR(penalty='SCAD'),ncvregR(penalty='MCP'),
-            #   sparsevbR(),varbvsR(),bayesregR(prior='hs')
+    models = [VEBGLM(prior='ash',tol=1e-6),
+              VEBGLM(prior='point_normal',tol=1e-6),
+              VEBGLM(prior='point_laplace',tol=1e-6),
+              elasticnetR(),
+              glmnetR(penalty="lasso"),
+              glmnetR(penalty="ridge"),
+              L0Learn(),
+              ncvregR(penalty='SCAD'),
+              ncvregR(penalty='MCP'),
+              sparsevbR(),
+              varbvsR()]
+              # bayesregR(prior='hs')
     
               
     metrics = [accuracy_score, precision_score, recall_score, f1_score,roc_auc_score]
@@ -49,14 +51,15 @@ def main(args):
     # Run simulation
     results = []
     datax = []
+    fitted_models = []
     for n in n_values:
         for p in p_values:
             for s in s_values:
                 for r in r_values:
-                    for rep in range(repetitions):
-                        for dist in dist_values:
+                    for dist in dist_values:
+                        for rep in range(repetitions):
                             print(f"running setting {n,p,s,r,dist}, rep {rep}")
-                            # beta = gen_beta_constant(n,p,r,s,target_var)
+                            start_t = timeit.default_timer()
                             beta = gen_beta(p,s,dist)
                             X_train, y_train = generate_train_data_binary(n, p, r, beta, X_generator)
                             X_test, y_test = generate_test_data_binary(args.n_test, p, r,beta, X_generator)
@@ -72,14 +75,25 @@ def main(args):
                                         'y_train':y_train,
                                         'X_test':X_test,
                                         'y_test':y_test})
-                            # with open(f"simulation/results/simu_data_{args.file_name}.pkl",'wb') as aa:
-                            #     pickle.dump(datax, aa)
+                            with open(f"simulation/results/simu_data_{args.file_name}.pkl",'wb') as aa:
+                                pickle.dump(datax, aa)
                             for model in models:
                                 try:
                                     model.fit(X_train, y_train)
                                     y_pred = model.predict(X_test)
                                     y_prob = model.predict_proba(X_test)
                                     model_name =  model.model_name
+                                    fitted_models.append({
+                                        'n': n,
+                                        'p': p,
+                                        'r': r,
+                                        's': s,
+                                        'beta_dist': dist,
+                                        'rep': rep,
+                                        'fitted_model': model,
+                                    })
+                                    with open(f"simulation/results/simu_fitted_model_{args.file_name}.pkl", "wb") as fp:
+                                        pickle.dump(fitted_models, fp)
                                     for metric in metrics:
                                         if metric.__name__ == 'roc_auc_score':
                                             score = metric(y_test, y_prob)
@@ -98,11 +112,13 @@ def main(args):
                                             'run_time': model.run_time
                                         })
                                     # save result after every modelling fitting
-                                    with open(f"simulation/results/simu_res_{args.file_name}.pkl", "wb") as fp:
+                                    with open(f"simulation/results/simu_metric_{args.file_name}.pkl", "wb") as fp:
                                         pickle.dump(results, fp)
                                 except Exception as e:
                                     print(f"Error with model {model.model_name}: {e}")
                                     continue
+                            end_t = timeit.default_timer()
+                            print(f"Took {end_t-start_t} seconds")
 
 
 
@@ -125,7 +141,7 @@ if __name__ == '__main__':
     parser.add_argument('--file_name', type=str, default='default', help='Number of repetitions for each setting')
     parser.add_argument('--beta_dist', type=str, default='normal', help='Number of repetitions for each setting')
     args = parser.parse_args()
-    datax, results = main(args)
+    _, results = main(args)
     # Analyze results
     results_df = pd.DataFrame(results)
     res = results_df.groupby(['model', 'metric','n','p','r','s','beta_dist']).mean().reset_index()
